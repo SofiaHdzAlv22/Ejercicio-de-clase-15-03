24f0e53638182f72abd260bc5b23f80af82b9d1d
SofiaHdzAlv22 <sofiahernandeza@iesantaelena.edu.co>
2023-03-16 15:41:20 -0500

    cambios

--- script/script.js
+++ script/script.js
@@ -1,35 +1,38 @@
 let nombre, correo, mensaje;
+
 let formulario = document.getElementById('form')
-formulario,addEventListener('submit',(e)=>{
+formulario, addEventListener('submit', (e) => {
     e.preventDefault()
     LeerDatos()
 })
 function LeerDatos() {
     nombre = document.getElementById('nombre').value
     correo = document.getElementById('correo').value
-    mensaje = document.querySelector('textarea').value
-    ValidarData(nombre,correo,mensaje)
-console.log(nombre)
-function ValidarData(nommbre,correo,mensaje)
-GuardarLocalStorage(nombre,correo,mensajes)
+    mensaje = document.querySelector('#textarea').value
+    ValidarData(nombre, correo, mensaje)
+    GuardarLocalStorage(nombre, correo, mensaje)
 }
 
- {
-    if(nombre,length==0 || correo.length==0 || mensaje.length==0){
-     Swal.fire({
-        title: 'Error',
-        text: 'Do you want to continue',
-        icon: 'Error',
-        confirmBurronText:'cool',
-        iconColor: 'purple',
-     })
+
+function ValidarData(nommbre, correo, mensaje) {
+    if (nombre.length == 0 || correo.length == 0 || mensaje.length == 0) {
+        Swal.fire({
+            title: 'Error',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmBurronText: 'cool',
+            iconColor: 'pink',
+        })
     }
+    GuardarLocalStorage(nombre, correo, mensaje)
 }
-function GuardarLocalStorage(nombre,correo,mensaje) {
-localStorage.setItem('Nombre',nombre)
-localStorage.setItem('Correo',correo)
-localStorage.setItem('Mensaje',mensaje)
-ListarData() 
+
+
+function GuardarLocalStorage(nombre, correo, mensaje) {
+    localStorage.setItem('Nombre', nombre)
+    localStorage.setItem('Correo', correo)
+    localStorage.setItem('Mensaje', mensaje)
+    ListarData()
 }
 
 function ListarData() {
